a7df42bbef1e7c5de3203059cb79dad306ae4490
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2019-04-20 11:55:01 -0400

    try vuetify & add csrf token

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>HackerPair</title>
 </head>
 <body>
-    @yield('content')
+
+    <div id="app">
+        <v-app>
+          <v-content>
+            <v-container>Hello world</v-container>
+            @yield('content')
+          </v-content>
+        </v-app>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
