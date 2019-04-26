9e62f792a8925a457437f545c7e2c4b3e4c99b6e
Serena Liao <serenaliaojc@gmail.com>
2019-04-26 13:55:48 -0400

    event api index & store

---
+++ app/Http/Controllers/API/EventsController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Hackerpair\Http\Controllers\API;
+
+use Hackerpair\Event;
+use Illuminate\Http\Request;
+use Hackerpair\Http\Controllers\Controller;
+
+class EventsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Event::take(5)->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $event = Event::create($request->input());
+        return response()->json([
+            'message' => 'Event created.'
+        ],200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \Hackerpair\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Event $event)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \Hackerpair\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Event $event)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Hackerpair\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Event $event)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Hackerpair\Event  $event
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Event $event)
+    {
+        //
+    }
+}
